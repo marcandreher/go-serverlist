3d8f25eaeb5d49277dfdc2fb632f049918c14e0e
Mempler <me@mempler.de>
2018-06-07 14:31:22 +0200

    Adding Config file.
    Signed-off-by: Mempler <me@mempler.de>

--- serverlist.go
+++ serverlist.go
@@ -1,9 +1,40 @@
 package main
 
-import "github.com/MarcPlaying/go-serverlist/server"
+import (
+	"os"
+
+	"github.com/Gigamons/common/consts"
+	"github.com/Gigamons/common/helpers"
+	"github.com/Gigamons/common/logger"
+	"github.com/MarcPlaying/go-serverlist/constants"
+	"github.com/MarcPlaying/go-serverlist/server"
+)
 
 func init() {
-
+	if _, err := os.Stat("config.yml"); os.IsNotExist(err) {
+		conf := &constants.Config{}
+		conf.MySQL = consts.MySQLConf{
+			Database: "serverlist",
+			Hostname: "0.0.0.0",
+			Port:     3306,
+			Username: "root",
+		}
+		conf.Server = struct {
+			Host  string
+			Port  int16
+			Debug bool
+		}{
+			Host: "0.0.0.0",
+			Port: 8745,
+		}
+		conf.Redis = consts.REDISConf{
+			Host: "127.0.0.1",
+			Port: 6379,
+		}
+		helpers.CreateConfig("config", conf)
+		logger.Info("I've just created a config for you! Please edit.")
+		os.Exit(0)
+	}
 }
 
 func main() {
